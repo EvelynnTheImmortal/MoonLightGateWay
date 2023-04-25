02236cccc70e362054f0fe1c11e71b19fecc87ff
Lithrill <107194454+Lithrill@users.noreply.github.com>
2023-04-25 11:09:38 +1000

    Fixed some issues on the tutorial Level
    tilemap colliders and scripting

--- Assets/Brendon/SCripts/DashWallDisable.cs
+++ Assets/Brendon/SCripts/DashWallDisable.cs
@@ -8,7 +8,8 @@ public class DashWallDisable : MonoBehaviour
     public int staminaThreshold = 33;
 
     public TopdownInputController2D tD;
-    
+
+    private bool isEPressed = false;
     //private void Awake()
     //{
     //    tD = GameObject.FindGameObjectWithTag("Player").GetComponent<TopdownInputController2D>();
@@ -20,10 +21,16 @@ public class DashWallDisable : MonoBehaviour
         {
             tD = GameObject.FindGameObjectWithTag("Player").GetComponent<TopdownInputController2D>();
         }
-        if (Input.GetKey(KeyCode.Space) && tD.currentStamina > staminaThreshold)
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isEPressed = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && isEPressed && tD.canDash && Time.timeScale != 0.0f && !GlobalStatus.freezePlayer && !GlobalStatus.freezeAll)
         {
             Debug.Log("this is working");
-            objectToDisable.GetComponent<BoxCollider2D>().enabled = false;
+            //objectToDisable.GetComponent<BoxCollider2D>().enabled = false;
+            objectToDisable.SetActive(false);
         }
         //if (Input.GetKey(KeyCode.Space) && GetComponent<TopdownInputController2D>().currentStamina < staminaThreshold)
         //{
